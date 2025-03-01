5246c55c2b038a1185d6bb82715d6f13f35ed904
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-03-02 02:38:37 +0530

    added gloabal variable if the environment is production

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 dotenv.config();
 
 const destinationRoutes = require('./routes/destinationRoutes');
